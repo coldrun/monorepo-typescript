e97d11058198d3498a2196d16daa8c7a5d46ec94
Rafal Galka <rgalka@coldrun.com>
2025-02-26 13:41:23 +0100

    style(nestjs): import fix

--- apps/nestjs/test/app.e2e-spec.ts
+++ apps/nestjs/test/app.e2e-spec.ts
@@ -1,8 +1,8 @@
 import { INestApplication } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import * as request from 'supertest';
-import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+import type { App } from 'supertest/types';
+import { AppModule } from '../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
@@ -21,9 +21,7 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+    expect(1).toBe(1);
+    return request(app.getHttpServer()).get('/').expect(200).expect('Hello World!');
   });
 });
